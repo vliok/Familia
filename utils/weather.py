e0c73c50e9e396d24df5c801e52621d76cfbac7a
Asher Lasday <asherlasday@Ashers-MacBook-Air.local>
2016-12-05 22:22:26 -0500

    linked to weather api and began work processing data

--- utils/weather.py
+++ utils/weather.py
@@ -1,1 +1,16 @@
-weather file
+import urllib2, json, urllib
+key = "cc75d8edb8caaac081449e4871136ffe"
+#https://api.darksky.net/forecast/[key]/[latitude],[longitude] (proper format)
+url = "https://api.darksky.net/forecast/cc75d8edb8caaac081449e4871136ffe/42.3601,-71.0589"
+
+request = urllib2.urlopen(url)
+result = request.read()
+d = json.loads(result)
+
+for key in d:
+    print key, 'corresponds to', d[key]
+    print 
+
+
+
+
