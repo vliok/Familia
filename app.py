57b1853efe54e44c6f8d5a245c3ff69f6d1d97cb
FelixRiegBaumhauer <friegbaumhauer@stuy.edu>
2016-12-05 22:47:35 -0500

    basic maps work, we need to rethink our adress input system

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import urllib2, json, os
+import urllib2, json, os, utils.maps
 
 app = Flask(__name__)
 
@@ -13,24 +13,11 @@ def home():
 @app.route("/results", methods=["GET","POST"])
 def overview():
     location = request.form["adress"]
-    print(location)
-    #NEED AFXN TO GO DOWN THE STR AND CHECK IF EACH OF THE elements are " "
-
+    
     if(location == "" or location == " "):
         return render_template("home.html", action_type="results", address=location, status="reenter")
 
-    map_code = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAJZ69TpmhYn_53GCf3f_UdTyu7TWAk4Mk&q="+location#"NewYork+NY"
-
-    split_location=location.split(",")
-    print(split_location)
-    print(len(split_location))
-    size=len(split_location)
-    
-    map_querry = ""+split_location[size-3]+"+"+split_location[size-2]+"+"+split_location[size-1]
-
-    print(map_querry)
-
-    #map_code = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAJZ69TpmhYn_53GCf3f_UdTyu7TWAk4Mk&q="+map_querry
+    map_code = utils.maps.get_map_querry(location);
     
     return render_template("home.html", action_type="results", address=location, status="show_info", get_map=map_code)
 
