4b35dbb8cf65a6fb26aed1c55b3f1ac8f74860c7
alasday <alasday@stuy.edu>
2016-12-07 14:00:38 -0500

    parsing data

--- utils/weather.py
+++ utils/weather.py
@@ -6,7 +6,7 @@ url = "https://api.darksky.net/forecast/cc75d8edb8caaac081449e4871136ffe/42.3601
 request = urllib2.urlopen(url)
 result = request.read()
 d = json.loads(result)
-
+'''
 for key in d:
     if (key == "hourly"): 
         for k2 in d[key]:
@@ -14,16 +14,29 @@ for key in d:
             print "corresponds to"
             print d[key][k2]
             print 
-
 #use icon to create a getBg fxn
-
+'''
 def backgrounder():
     for key in d:
         if (key == "hourly"):
             for k2 in d[key]:
                 if (k2 == "icon"):
-                    print k2
-                    print "corresponds to"
+                    
                     print d[key][k2]
-                    print
+                    return d[key][k2]
 
+                
+def temp():
+    for key in d:
+        if (key == "hourly"):
+            for k2 in d[key]:
+                if (k2 == "data"):
+                    print d[key][k2][0]
+                    #return d[key][k2]
+
+
+            
+backgrounder()
+print()
+print()
+temp()
