80088edfdc27f5a41f02dbb831cded7aab06f962
FelixRiegBaumhauer <friegbaumhauer@stuy.edu>
2016-12-06 19:46:32 -0500

    added an error mesage if the geolocator is uncorrect

--- app.py
+++ app.py
@@ -25,16 +25,20 @@ def overview():
         lat = float(request.form["lat"])
         lon = float(request.form["lon"])
         location = utils.maps.reverse_geo({'lat': lat, 'lng': lon})
+        view_location = location #making sure that it is defined
+        askIfCor=True#a way for us to ask our user if their adress is correct
     except:
         #if manually typing address
-        location = request.form["adress"].replace(" ", "+")
+        view_location=request.form["adress"]
+        location = view_location.replace(" ", "+")
+        askIfCor=False
 
     if(location == "" or location == " "):
-        return render_template("home.html", action_type="results", address=location, status="reenter")
+        return render_template("home.html", action_type="results", address=location, view_address=view_location, status="reenter")
 
     map_code = utils.maps.get_map_query(location);
 
-    return render_template("home.html", action_type="results", address=location, status="show_info", get_map=map_code)
+    return render_template("home.html", action_type="results", address=location, view_address=view_location, status="show_info", get_map=map_code, askIfCorrect=askIfCor)
 
 
 if __name__ == "__main__":
