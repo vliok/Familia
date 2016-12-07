02a91d71647b997d77a84b262c217d90a444b63b
Vincent Liok <vliok@stuy.edu>
2016-12-07 14:04:29 -0500

    function in movies.py returns list of nearby movies

--- utils/movies.py
+++ utils/movies.py
@@ -3,10 +3,12 @@ import urllib2, json, os, urllib
 key = "Q2ZBILRSORPXPLVBNGTX2BOBOYYBPPBBL4KBJDGKY0GEI35F&v=20161206"
 
 def get_movies(lat,lon):
-    url = "https://api.foursquare.com/v2/venues/explore?ll="+str(lat)+","+str(lon)+"&query=movie&limit=1&oauth_token="+key
+    url = "https://api.foursquare.com/v2/venues/explore?ll="+str(lat)+","+str(lon)+"&query=movie&limit=5&oauth_token="+key
     request = urllib2.urlopen(url)
     result = request.read()
     d = json.loads(result)
-    print d
+    d = d["response"]["groups"][0]["items"]
+    for venue in d:
+        print venue["venue"]["name"]
 
 get_movies(40.3,-74)
