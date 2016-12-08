56f30d640126b0cd3f1e13d51dd42c3f5d483ddb
Vincent Liok <vliok@stuy.edu>
2016-12-07 22:07:16 -0500

    display nearby movie theaters

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import urllib2, json, os, utils.maps, utils.foursquare
+import urllib2, json, os, utils.maps, utils.foursquare, utils.movies
 
 app = Flask(__name__)
 
@@ -47,7 +47,10 @@ def overview():
     queries = ["food", "coffee", "shops", "outdoors"]
     for q in queries:
         venueList.append(utils.foursquare.get_venues(lat, lon, q))
-        
+    
+    #movies section    
+    queries.append("movie theaters")
+    venueList.append(utils.movies.get_movies(lat, lon, 4))
 
     return render_template("home.html", action_type="results", address=location, view_address=view_location, status="show_info", get_map=map_code, askIfCorrect=askIfCor, venues=venueList, q=queries)
 
